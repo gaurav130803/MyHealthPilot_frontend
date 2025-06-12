8a444e553199666faa8befd9585fa527f946fe46
gaurav chopra <chopragc03@gmail.com>
2025-06-12 15:00:19 +0530

    added backend usrl

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-    plugins: [react(),tailwindcss()],
-  
-})
+  plugins: [react(),tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    allowedHosts: ['myhealthpilot-backend.onrender.com']
+
+  }
+});
