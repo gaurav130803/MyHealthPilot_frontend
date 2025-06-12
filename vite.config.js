e11a27e556bf677e953e3f2c19498cb3ccc9e1de
gaurav chopra <chopragc03@gmail.com>
2025-06-12 15:03:10 +0530

    added backend usrl

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
-    allowedHosts: ['myhealthpilot-backend.onrender.com']
+    allowedHosts: ['myhealthpilot-frontend.onrender.com']
 
   }
 });
